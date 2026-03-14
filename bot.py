207f0527374a4cffebc94df005f3465c250c3fb3
keshablack <71798323+keshablack@users.noreply.github.com>
2026-03-15 02:44:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -727,8 +727,11 @@ def generate_map():
 
 # ===== РУЛЕТКА ₽ =====
 
-@bot.message_handler(func=lambda m: m.text and m.text.isdigit())
+@bot.message_handler(func=lambda m: m.text)
 def roulette_handler(m):
+
+    if not m.text.isdigit():
+        return
 
     p = get_player(m.from_user)
     num = int(m.text)
